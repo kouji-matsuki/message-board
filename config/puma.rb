35ea1b7a89229cfd62136665dc2b12101ec91fc8
kouji-matsuki <boranchimk1216@gmail.com>
2022-08-13 13:50:59 +0000

    removed node_modules & yarn add @rails/activestorage

--- config/puma.rb
+++ config/puma.rb
@@ -18,7 +18,9 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
+#
 environment ENV.fetch("RAILS_ENV") { "development" }
+
 # Specifies the `pidfile` that Puma will 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
